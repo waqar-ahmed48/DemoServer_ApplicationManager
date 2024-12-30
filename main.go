2f919552f0af8e81f3c213ca5e0ddc3fd96d7ae4
waqar-ahmed48 <waqar.ahmed@gmail.com>
2024-12-30 01:13:28 -0500

    starting to add tofu operations entries

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ import (
 	"syscall"
 	"time"
 
-	"DemoServer_APPLICATIONMANAGER/configuration"
-	"DemoServer_APPLICATIONMANAGER/datalayer"
-	"DemoServer_APPLICATIONMANAGER/handlers"
-	"DemoServer_APPLICATIONMANAGER/otel"
-	"DemoServer_APPLICATIONMANAGER/secretsmanager"
+	"DemoServer_ApplicationManager/configuration"
+	"DemoServer_ApplicationManager/datalayer"
+	"DemoServer_ApplicationManager/handlers"
+	"DemoServer_ApplicationManager/otel"
+	"DemoServer_ApplicationManager/secretsmanager"
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/gorilla/mux"
@@ -103,8 +103,6 @@ func main() {
 		err = errors.Join(err, otelShutdown(context.Background()))
 	}()
 
-	//r.Use(otelmux.Middleware(cfg.Server.PrefixMain))
-
 	pd, err := datalayer.NewPostgresDataSource(&cfg, l)
 	if err != nil {
 		l.Error("PostgresDataSource initialization failed. Error: " + err.Error())
@@ -123,58 +121,142 @@ func main() {
 		os.Exit(2)
 	}
 
-	ch, err := handlers.NewConnectionsHandler(&cfg, l, pd)
-	if err != nil {
-		l.Error("Connections Handler initialization failed. Error: " + err.Error())
-		os.Exit(2)
-	}
-
 	sh := handlers.NewStatusHandler(l, pd, &cfg)
 
 	statusRouter := r.Methods(http.MethodGet).Subrouter()
-	statusRouter.HandleFunc("/v1/connectionmgmt/status", sh.GetStatus)
+	statusRouter.HandleFunc("/v1/applicationmgmt/status", sh.GetStatus)
 	statusRouter.Use(otelhttp.NewMiddleware("GET /status"))
 
-	cGetRouter := r.Methods(http.MethodGet).Subrouter()
-	cGetRouter.HandleFunc("/v1/connectionmgmt/connections", ch.GetConnections)
-	cGetRouter.Use(otelhttp.NewMiddleware("GET /connections"))
-	cGetRouter.Use(ch.MiddlewareValidateConnectionsGet)
-
-	jch, err := handlers.NewAWSConnectionHandler(&cfg, l, pd, vh)
+	ah, err := handlers.NewApplicationsHandler(&cfg, l, pd, vh)
 	if err != nil {
-		l.Error("AWSConnectionHandler initialization failed. Error: " + err.Error())
+		l.Error("ApplicationsHandler initialization failed. Error: " + err.Error())
 		os.Exit(2)
 	}
 
-	jcGetConnectionsRouter := r.Methods(http.MethodGet).Subrouter()
-	jcGetConnectionsRouter.HandleFunc("/v1/connectionmgmt/connections/aws", jch.GetAWSConnections)
-	jcGetConnectionsRouter.Use(otelhttp.NewMiddleware("GET /connections/aws"))
-	jcGetConnectionsRouter.Use(jch.MiddlewareValidateAWSConnectionsGet)
+	getAppRouter := r.Methods(http.MethodGet).Subrouter()
+	getAppRouter.HandleFunc("/v1/applicationmgmt/applications", ah.GetApplications)
+	getAppRouter.Use(otelhttp.NewMiddleware("GET /applications"))
+	getAppRouter.Use(ah.MiddlewareValidateApplicationsGet)
 
-	jcGetRouterWithID := r.Methods(http.MethodGet).Subrouter()
-	jcGetRouterWithID.HandleFunc("/v1/connectionmgmt/connection/aws/{connectionid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}", jch.GetAWSConnection)
-	jcGetRouterWithID.Use(otelhttp.NewMiddleware("GET /connection/aws"))
-	jcGetRouterWithID.Use(jch.MiddlewareValidateAWSConnection)
+	getAppRouterWithID := r.Methods(http.MethodGet).Subrouter()
+	getAppRouterWithID.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}", ah.GetApplication)
+	getAppRouterWithID.Use(otelhttp.NewMiddleware("GET /application"))
+	getAppRouterWithID.Use(ah.MiddlewareValidateApplication)
 
-	jcTestRouterWithID := r.Methods(http.MethodGet).Subrouter()
-	jcTestRouterWithID.HandleFunc("/v1/connectionmgmt/connection/aws/test/{connectionid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}", jch.TestAWSConnection)
-	jcTestRouterWithID.Use(otelhttp.NewMiddleware("GET /connection/aws/test"))
-	jcTestRouterWithID.Use(jch.MiddlewareValidateAWSConnection)
+	postAppRouter := r.Methods(http.MethodPost).Subrouter()
+	postAppRouter.HandleFunc("/v1/applicationmgmt/application", ah.AddApplication)
+	postAppRouter.Use(otelhttp.NewMiddleware("POST /application"))
+	postAppRouter.Use(ah.MiddlewareValidateApplicationPost)
 
-	jcPostRouter := r.Methods(http.MethodPost).Subrouter()
-	jcPostRouter.HandleFunc("/v1/connectionmgmt/connection/aws", jch.AddAWSConnection)
-	jcPostRouter.Use(otelhttp.NewMiddleware("POST /connection/aws"))
-	jcPostRouter.Use(jch.MiddlewareValidateAWSConnectionPost)
+	patchAppRouter := r.Methods(http.MethodPatch).Subrouter()
+	patchAppRouter.HandleFunc("/v1/applicationmgmt/application{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}", ah.UpdateApplication)
+	patchAppRouter.Use(otelhttp.NewMiddleware("PATCH /application"))
+	patchAppRouter.Use(ah.MiddlewareValidateApplicationUpdate)
 
-	jcPatchRouter := r.Methods(http.MethodPatch).Subrouter()
-	jcPatchRouter.HandleFunc("/v1/connectionmgmt/connection/aws/{connectionid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}", jch.UpdateAWSConnection)
-	jcPatchRouter.Use(otelhttp.NewMiddleware("PATCH /connection/aws"))
-	jcPatchRouter.Use(jch.MiddlewareValidateAWSConnectionUpdate)
+	deleteAppRouter := r.Methods(http.MethodDelete).Subrouter()
+	deleteAppRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}", ah.DeleteApplication)
+	deleteAppRouter.Use(otelhttp.NewMiddleware("DELETE /application"))
+	deleteAppRouter.Use(ah.MiddlewareValidateAWSConnection)
 
-	jcDeleteRouter := r.Methods(http.MethodDelete).Subrouter()
-	jcDeleteRouter.HandleFunc("/v1/connectionmgmt/connection/aws/{connectionid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}", jch.DeleteAWSConnection)
-	jcDeleteRouter.Use(otelhttp.NewMiddleware("DELETE /connection/aws"))
-	jcDeleteRouter.Use(jch.MiddlewareValidateAWSConnection)
+	postTofuPackageUploadRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuPackageUploadRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/package", ah.TofuUploadPackage)
+	postTofuPackageUploadRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/package"))
+	postTofuPackageUploadRouter.Use(ah.MiddlewareValidateTofuPackageUpload)
+
+	getTofuPackageDownloadRouter := r.Methods(http.MethodGet).Subrouter()
+	getTofuPackageDownloadRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/package", ah.TofuDownloadPackage)
+	getTofuPackageDownloadRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/package"))
+	getTofuPackageDownloadRouter.Use(ah.MiddlewareValidateTofuPackageDownload)
+
+	postTofuPackagePackRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuPackagePackRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/package/pack", ah.TofuPackPackage)
+	postTofuPackagePackRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/package/pack"))
+	postTofuPackagePackRouter.Use(ah.MiddlewareValidateTofuPackagePack)
+
+	postTofuPackageUnpackRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuPackageUnpackRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/package/unpack", ah.TofuUnpackPackage)
+	postTofuPackageUnpackRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/package/unpack"))
+	postTofuPackageUnpackRouter.Use(ah.MiddlewareValidateTofuPackageUnpack)
+
+	postTofuInitkRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuInitkRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/init", ah.TofuInit)
+	postTofuInitkRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/init"))
+	postTofuInitkRouter.Use(ah.MiddlewareValidateTofuInit)
+
+	postTofuValidateRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuValidateRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/validate", ah.TofuValidate)
+	postTofuValidateRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/validate"))
+	postTofuValidateRouter.Use(ah.MiddlewareValidateTofuValidate)
+
+	postTofuPlanRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuPlanRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/plan", ah.TofuPlan)
+	postTofuPlanRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/plan"))
+	postTofuPlanRouter.Use(ah.MiddlewareValidateTofuPlan)
+
+	postTofuApplyRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuApplyRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/apply", ah.TofuApply)
+	postTofuApplyRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/apply"))
+	postTofuApplyRouter.Use(ah.MiddlewareValidateTofuApply)
+
+	postTofuDestroyRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuDestroyRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/destroy", ah.TofuDestroy)
+	postTofuDestroyRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/destroy"))
+	postTofuDestroyRouter.Use(ah.MiddlewareValidateTofuDestroy)
+
+	getTofuShowRouter := r.Methods(http.MethodGet).Subrouter()
+	getTofuShowRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/plan", ah.TofuPlan)
+	getTofuShowRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/plan"))
+	getTofuShowRouter.Use(ah.MiddlewareValidateTofuPlan)
+
+	getTofuStateRouter := r.Methods(http.MethodGet).Subrouter()
+	getTofuStateRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/state", ah.TofuState)
+	getTofuStateRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/state"))
+	getTofuStateRouter.Use(ah.MiddlewareValidateTofuState)
+
+	deleteTofuStateResourceRouter := r.Methods(http.MethodDelete).Subrouter()
+	deleteTofuStateResourceRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/state/resource", ah.DeleteTofuStateResource)
+	deleteTofuStateResourceRouter.Use(otelhttp.NewMiddleware("DELETE /application/tofu/state/resource"))
+	deleteTofuStateResourceRouter.Use(ah.MiddlewareValidateDeleteTofuStateResource)
+
+	postTofuStateResourceRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuStateResourceRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/state/resource", ah.AddTofuStateResource)
+	postTofuStateResourceRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/state/resource"))
+	postTofuStateResourceRouter.Use(ah.MiddlewareValidatePostTofuStateResource)
+
+	postTofuModuleRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuModuleRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/module", ah.TofuModule)
+	postTofuModuleRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/module"))
+	postTofuModuleRouter.Use(ah.MiddlewareValidatePostTofuModule)
+
+	getTofuWorkspaceRouter := r.Methods(http.MethodGet).Subrouter()
+	getTofuWorkspaceRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/workspaces", ah.GetTofuWorkspaces)
+	getTofuWorkspaceRouter.Use(otelhttp.NewMiddleware("GET /application/tofu/workspaces"))
+	getTofuWorkspaceRouter.Use(ah.MiddlewareValidateGetTofuWorkspaces)
+
+	postTofuWorkspaceRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuWorkspaceRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/workspace", ah.AddTofuWorkspace)
+	postTofuWorkspaceRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/workspace"))
+	postTofuWorkspaceRouter.Use(ah.MiddlewareValidatePostTofuWorkspace)
+
+	postTofuWorkspaceSwitchRouter := r.Methods(http.MethodPost).Subrouter()
+	postTofuWorkspaceSwitchRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/workspace/switch", ah.SwitchTofuWorkspace)
+	postTofuWorkspaceSwitchRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/workspace/switch"))
+	postTofuWorkspaceSwitchRouter.Use(ah.MiddlewareValidateTofuWorkspaceSwitch)
+
+	getTofuGraphRouter := r.Methods(http.MethodGet).Subrouter()
+	getTofuGraphRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/graph", ah.TofuGraph)
+	getTofuGraphRouter.Use(otelhttp.NewMiddleware("POST /application/tofu/graph"))
+	getTofuGraphRouter.Use(ah.MiddlewareValidateTofuGraph)
+
+	getTofuOutputRouter := r.Methods(http.MethodGet).Subrouter()
+	getTofuOutputRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/output", ah.TofuOutput)
+	getTofuOutputRouter.Use(otelhttp.NewMiddleware("GET /application/tofu/output"))
+	getTofuOutputRouter.Use(ah.MiddlewareValidateTofuOutput)
+
+	getTofuVersionRouter := r.Methods(http.MethodGet).Subrouter()
+	getTofuVersionRouter.HandleFunc("/v1/applicationmgmt/application/{applicationid:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$}/tofu/version", ah.TofuVersion)
+	getTofuVersionRouter.Use(otelhttp.NewMiddleware("GET /application/tofu/version"))
+	getTofuVersionRouter.Use(ah.MiddlewareValidateTofuVersion)
 
 	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
 	docs_sh := middleware.Redoc(opts, nil)
